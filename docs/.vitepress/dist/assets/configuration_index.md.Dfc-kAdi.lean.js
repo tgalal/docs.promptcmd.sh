da7898a2688b33c5144873f118a7e3a0996b8594
Tarek Galal <tare2.galal@gmail.com>
2026-01-19 16:01:27 +0100

    Fix configuratioin

--- docs/.vitepress/dist/assets/configuration_index.md.Dfc-kAdi.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as n}from"./chunks/framework.BJEpWshW.js";const _=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"configuration/index.md","filePath":"configuration/index.md"}'),a={name:"configuration/index.md"};function o(r,i,c,s,d,f){return n(),t("div")}const m=e(a,[["render",o]]);export{_ as __pageData,m as default};
